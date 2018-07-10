2aa209b1e8157a753295d6833d9984366f6c6317
Calvin Hu <cahu@ucdavis.edu>
2018-07-09 21:25:56 -0700

    Section 5, Lecture 66

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// Thanks to webpack, we can "import" css files in js files
 import './App.css';
 import Person from './Person/Person';
 
@@ -8,30 +9,32 @@ class App extends Component {
   // @state is only available in stateful components, or components that use @Component.
   // Use functional over stateful components whenever possible for best practice.
   // If @state changes, React will re-render the necessary components and potentially update the DOM in the browser
+  // By adding @id to each person object, React will be more efficient in finding and deleting a person from the list
   state = {
     persons: [
-      { name: 'Calvin', age: 22 },
-      { name: 'Kevin', age: 23 },
-      { name: 'Bob', age: 24 },
+      { id: 'a', name: 'Calvin', age: 22 },
+      { id: 'b', name: 'Kevin', age: 23 },
+      { id: 'c', name: 'Bob', age: 24 },
     ],
     otherState: 'Some other value',
+    showPersons: false,
   }
 
-  // switchName can be whatever, but Handler is good convention!
-  switchNameHandler = (newName) => {
-    // Don't do this to change state, React won't recognize it.
-    // this.state.persons[0].name = 'Calvin Hu';
+  // // switchName can be whatever, but Handler is good convention!
+  // switchNameHandler = (newName) => {
+  //   // Don't do this to change state, React won't recognize it.
+  //   // this.state.persons[0].name = 'Calvin Hu';
 
-    // Using this correct way, React will only change state of the elements that are changed.
-    // In other words, @otherState will NOT be touched, only @persons will be.
-    this.setState({
-      persons: [
-        { name: newName, age: 24 },
-        { name: 'Kevin Fang', age: 25 },
-        { name: 'Bob Ross', age: 26 },
-      ],
-    })
-  }
+  //   // Using this correct way, React will only change state of the elements that are changed.
+  //   // In other words, @otherState will NOT be touched, only @persons will be.
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: 24 },
+  //       { name: 'Kevin Fang', age: 25 },
+  //       { name: 'Bob Ross', age: 26 },
+  //     ],
+  //   })
+  // }
 
   // The below function will have problems with using this.
   // The function assigned to the name as a property above will not have this problem.
@@ -39,45 +42,112 @@ class App extends Component {
     // this.propertyNameHere
   // }
 
-  nameChangeHandler = (event) => {
-    // @event.target refers to @input (input.value)
+  nameChangeHandler = (event, id) => {
+    // @personIndex will hold the index of the person in the state where p.id equals id
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // Create a copy of the found person object
+    const person = {...this.state.persons[personIndex]};
+    // update the person with the new value
+    person.name = event.target.value;
+
+    // Alternative way (not as modern)
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    // copy of all persons
+    const persons = [...this.state.persons];
+    // update the persons array with the new person that was updated
+    persons[personIndex] = person;
+
+    // set state with mutated copy
+    this.setState({ persons: persons })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // Arrays and objects are reference types, so we are only change the element @persons is pointing to
+    // const persons = this.state.persons.slice();
+    // Below line of code is bad practice because we are mutating the underlying @persons in @state of App.js
+    // A good practice is creating a copy using the @slice function (as shown above)
+    // An alternative way is to use the spread (...) operator which is the more modern way (as shown above)
+    // In general, you should always update state in an IMMUTABLE way. In other words, you should not mutate the original state first before the update.
+    // Create a copy, and change that, then set state.
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  }
+
+  togglePersonsHandler = () => {
     this.setState({
-      persons: [
-        { name: 'Calvin Hu', age: 24 },
-        { name: event.target.value, age: 25 },
-        { name: 'Bob Ross', age: 26 },
-      ],
+      showPersons: !this.state.showPersons
     })
   }
 
   render() {
+    // Styling hovering effect is pretty hard using inline styles
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      // :hover is a Radium feature (npm install --save radium) in project root folder
+      // ':hover': {
+      //   backgroundColor: 'lightgreen',
+      //   color: 'black',
+      // },
+    };
+
+    let persons = null;
+
+    // Below is common way to show lists in React using map
+    // @index is an automatically passed in argument in the @map function
+    // @index is not a good key because if a new person is added then the index for all subsequent persons will change
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              name={person.name} 
+              age={person.age} 
+              click={() => this.deletePersonHandler(index)}
+              key={person.id}
+              // must have @event defined explicitly on the LHS in the anonymous function, and then passed to the @nameChangedHandler on the RHS
+              changed={(event) => this.nameChangeHandler(event, person.id)} />
+          })}
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = []; // "red bold"
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
+    
     // This is NOT "HTML", this is JSX. It is syntactic sugar, and compiles down to JavaScript.
     // React is not using the real @div and @h1 tags, it handles these behind the scenes.
+    // Best practice to wrap everything into one root div element
+    // Must wrap entire app in @<StyleRoot> if using advanced features like media queries
     return (
       <div className="App">
         <h1>Hi, Calvin!</h1>
-        <p>A paragraph</p>
+        <p className={classes.join(' ')}>A paragraph</p>
         {/* Do not type "this.switchNameHandler()", React will call the function immediately on render */}
         {/* For arrow functions, an implicit @return is added after the arrow */}
         {/* We are passing an anonymous function for @onClick */}
         {/* Use @bind more often, since anonymouse function can be inefficient. */}
-        <button onClick={() => this.switchNameHandler('Calvin Hu was binded once!')} >Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age} 
-          // Here, we pass a reference to @switchNameHandler to @Person.
-          // @bind will control what the @this inside @switchNameHandler will refer to.
-          click={this.switchNameHandler.bind(this, 'Calvin Hu was binded twice!')}
-          changed={this.nameChangeHandler} >My Hobbies: Racing</Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler} >Toggle Persons</button>
+        {persons}
       </div>
-      // Best practice to wrap everything into one root div element
-      // <h1>Another heading</h1>
     );
     // JSX code above compiles to this line below.
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Calvin!'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import './Person.css';
+
 // Use this function form of component as often as possible because these are very clear about what they do.
 // They are simple, they don't manipulate the application.
 // Most components just render.
 // How do you "listen" in person, but have @App.js change state itself, without converting @Person.js into a class?
 const person = (props) => {
     return (
-        <div>
+        // @style will override @className styles on behalf of regular CSS rules, not because of Radium rules
+        <div className='Person' >
             {/* Attributes of the object */}
             {/* This @onClick is passed a reference to a function that resides in @App.js. This is a common pattern. */}
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
